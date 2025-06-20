6be9ffb34f9d68191f3489f16e5b44775889146d
Regis Camimura <regiscamimura@gmail.com>
2025-06-20 14:54:07 -0300

    Added basic skeleton

---
+++ agent_style_transfer/__init__.py
@@ -0,0 +1,11 @@
+"""
+Style Transfer Agent
+
+A standalone Python agent that implements textual style transfer 
+from pre-processed documents using the Arcade.dev ecosystem.
+"""
+
+from .schemas import ContentType, Document
+
+__version__ = "0.1.0"
+__all__ = ["ContentType", "Document"] 
